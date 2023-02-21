cf9f71a2f35bf5398a893b0d5d51b6fb58da77ba
Zhenya Sikirzhitsky <zhenya_sikirzhitsky@epam.com>
2023-02-21 23:43:02 +0200

    main: working wc

--- src/ch3/ls.js
+++ src/ch3/ls.js
@@ -8,27 +8,13 @@ const files = await glob(`${srcDir}/**/*.*`, { ignore: ['*.bck', './node_modules
 const content = displayFn => async (fileName) => displayFn(fileName, await readFile(fileName, 'utf8').catch(e => console.log(e)));
 const info = (fileName, content) => console.log(`File ${fileName} has ${content.length} characters, ${content.split('\n').length} lines and ${content.split(' ').length} words.`);
 
-// await Promise
-//   .allSettled(files
-//     .flatMap(async (file) => await Promise.all(
-//       [{ file, stats: await stat(file) }]
-//       .filter(({ stats }) => { stats.isFile() })
-//       .map(({ file }) => file)
-//     ))
-//     .map(content(info))
-//   );
-const result = await Promise.all(files.map(async file => ({ file, isFile: (await stat(file)).isFile() })));
 
-console.log(result);
-//
-// const filteredResults = result.filter(({ isFile }) => isFile).map(({ file }) => file);
-//
-// const view = await Promise.all(filteredResults.map(content(info)))
+const result = await Promise.all(files
+  .map(async file => [{ file, isFile: (await stat(file)).isFile() }]
+    .filter(({ isFile }) => isFile)
+    .map(({ file }) => file)
+  )
+);
 
-// console.log(view);
+await Promise.all(result.flat().map(content(info)));
 
-
-
-// const view = await Promise.all(result.map(async (file) => await readFile(file, 'utf8') ));
-
-// console.log(view);
