cff58faf4956e90d1bda0479b7790b0a8a697d95
Zhenya Sikirzhitsky <zhenya_sikirzhitsky@epam.com>
2023-02-27 22:30:56 +0200

    main: updated backup

--- src/ch5/check_backup.js
+++ src/ch5/check_backup.js
@@ -1,3 +1,9 @@
 import backup from './backup.js';
 
-await backup('./backup_2', './backup_2');
+const [src, dst] = process.argv.slice(2);
+if (!src || !dst) {
+  console.error('Usage: node backup.js <source> <destination>');
+  process.exit(1);
+}
+
+await backup(src, dst);
